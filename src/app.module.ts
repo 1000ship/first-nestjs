aa353a7d4e53edb3c14df908720da115c2acd5f9
1000ship <cjstjdgur1234@gmail.com>
2021-01-03 15:02:09 +0900

    feat: Generate websocket chats

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { UserModule } from './user/user.module';
 import { PostsModule } from './posts/posts.module';
+import { ChatsModule } from './chats/chats.module';
 
 @Module({
-  imports: [ProductsModule, UserModule, PostsModule], // 만들었던 ProductsModule 이 여기서 임포트된다.
+  imports: [ProductsModule, UserModule, PostsModule, ChatsModule], // 만들었던 ProductsModule 이 여기서 임포트된다.
   controllers: [AppController],
   providers: [AppService],
 })
