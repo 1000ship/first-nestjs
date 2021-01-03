22ee9556ee67cfb0061f1ee605aa9741e2e4f348
1000ship <cjstjdgur1234@gmail.com>
2021-01-03 14:58:36 +0900

    feat: Generate resource post

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { UserModule } from './user/user.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [ProductsModule, UserModule], // 만들었던 ProductsModule 이 여기서 임포트된다.
+  imports: [ProductsModule, UserModule, PostsModule], // 만들었던 ProductsModule 이 여기서 임포트된다.
   controllers: [AppController],
   providers: [AppService],
 })
