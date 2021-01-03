0755979b556527b304ab5ab62424b6c1c8cc593a
1000ship <cjstjdgur1234@gmail.com>
2021-01-03 15:04:39 +0900

    feat: Generate resource graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { ProductsModule } from './products/products.module';
 import { UserModule } from './user/user.module';
 import { PostsModule } from './posts/posts.module';
 import { ChatsModule } from './chats/chats.module';
+import { GraphqlsModule } from './graphqls/graphqls.module';
 
 @Module({
-  imports: [ProductsModule, UserModule, PostsModule, ChatsModule], // 만들었던 ProductsModule 이 여기서 임포트된다.
+  imports: [ProductsModule, UserModule, PostsModule, ChatsModule, GraphqlsModule], // 만들었던 ProductsModule 이 여기서 임포트된다.
   controllers: [AppController],
   providers: [AppService],
 })
